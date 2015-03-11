11321dfead6cb3c12431cf8fd6ac8635a12840bd
shamHu <shamHu@gmail.com>
2015-03-10 22:35:42 -0700

    Initial Commit
    Middle line rotating in sync with hour hand

---
+++ src/feature_gpath.c
@@ -0,0 +1,96 @@
+#include "pebble.h"
+
+// This defines graphics path information to be loaded as a path later
+static const GPathInfo LINE_PATH_POINTS = {
+  // This is the amount of points
+  4,
+  // A path can be concave, but it should not twist on itself
+  // The points should be defined in clockwise order due to the rendering
+  // implementation. Counter-clockwise will work in older firmwares, but
+  // it is not officially supported
+  (GPoint []) {
+    {-5, 125},
+    {5, 125},
+    {2, -125},
+	{-2, -125}
+  }
+};
+
+static Window *s_main_window;
+static Layer *s_path_layer;
+
+static GPath *s_path_array[NUM_PATHS];
+static GPath *s_line_path;
+
+static int s_current_path_index;
+static int s_path_angle;
+static bool s_outline_mode;
+
+// This is the layer update callback which is called on render updates
+static void path_layer_update_callback(Layer *layer, GContext *ctx) {
+	// You can rotate the path before rendering
+	time_t tempTime = time(NULL);
+	struct tm * timeStruct = localtime(&tempTime);
+
+	s_path_angle = (((timeStruct->tm_hour % 12) * 60) + timeStruct->tm_min) * 360 / (12 * 60);
+
+	gpath_rotate_to(s_line_path, (TRIG_MAX_ANGLE / 360) * s_path_angle);
+
+	graphics_context_set_stroke_color(ctx, GColorWhite);
+	gpath_draw_outline(ctx, s_line_path);
+}
+
+static int path_angle_add(int angle) {
+	return s_path_angle = (s_path_angle + angle) % 360;
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+	time_t tempTime = time(NULL);
+	struct tm * timeStruct = localtime(&tempTime);
+
+	s_path_angle = (((15 % 12) * 60) + timeStruct->tm_min) * 360 / (12 * 60);
+}
+
+static void main_window_load(Window *window) {
+	Layer *window_layer = window_get_root_layer(window);
+	GRect bounds = layer_get_frame(window_layer);
+
+	s_path_layer = layer_create(bounds);
+	layer_set_update_proc(s_path_layer, path_layer_update_callback);
+	layer_add_child(window_layer, s_path_layer);
+
+	// Move all paths to the center of the screen
+	gpath_move_to(s_line_path, GPoint(bounds.size.w/2, bounds.size.h/2));
+}
+
+static void main_window_unload(Window *window) {
+	layer_destroy(s_path_layer);
+}
+
+static void init() {
+	// Pass the corresponding GPathInfo to initialize a GPath
+	s_line_path = gpath_create(&LINE_PATH_POINTS);
+
+	tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+	
+	// Create Window
+	s_main_window = window_create();
+	window_set_background_color(s_main_window, GColorBlack);
+	window_set_window_handlers(s_main_window, (WindowHandlers) {
+		.load = main_window_load,
+		.unload = main_window_unload,
+	});
+	window_stack_push(s_main_window, true);
+}
+
+static void deinit() {
+	window_destroy(s_main_window);
+
+	gpath_destroy(s_line_path);
+}
+
+int main(void) {
+	init();
+	app_event_loop();
+	deinit();
+}
